2fb0ff4d234b650c255a9d5ba8e1a975ee70adbf
Luis Higuera <luis.higu3ra@gmail.com>
2023-03-17 11:45:39 -0600

    Agregar archivos de proyecto.

---
+++ investing/FRM/FRMCLIENTES.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace investing.FRM
+{
+    public partial class FRMCLIENTES : Form
+    {
+        public FRMCLIENTES()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
