962c95fd86cf6f49742b92637b17f3099cf9dada
Adair Moreno <morenoadair724@gmail.com>
2023-03-21 10:50:19 -0700

    FRMTERMINADO

--- investing/FRM/FRMUNIDAD.cs
+++ investing/FRM/FRMUNIDAD.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace investing
+namespace investing.FRM
 {
-    public partial class Form1 : Form
+    public partial class FRMUNIDAD : Form
     {
-        public Form1()
+        public FRMUNIDAD()
         {
             InitializeComponent();
         }
